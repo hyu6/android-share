88f8b43596e9c048f9f0e7101b72c34e0fb81c52
kalinjiang <jiang.ka@huaky.neu.edu>
2020-07-21 20:48:15 -0700

    fix displaying error

--- app/src/main/java/edu/neu/madcourse/share/Fragment/HomeFragment.java
+++ app/src/main/java/edu/neu/madcourse/share/Fragment/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -91,7 +92,7 @@ public class HomeFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 followings.clear();
                 for(DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    followings.add(snapshot.getKey());
+                    followings.add(dataSnapshot.getKey());
                 }
 
                 getPosts();
