1adf7491dc5b15c852f3bed5a5dbfbdba8263fb7
kalinjiang <jiang.ka@huaky.neu.edu>
2020-07-26 15:03:17 -0700

    set up logout

--- app/src/main/java/edu/neu/madcourse/share/SettingsActivity.java
+++ app/src/main/java/edu/neu/madcourse/share/SettingsActivity.java
@@ -3,10 +3,15 @@ package edu.neu.madcourse.share;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
+    private TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,16 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(SettingsActivity.this, StartActivity.class);
+                startActivity(intent);
             }
         });
     }
