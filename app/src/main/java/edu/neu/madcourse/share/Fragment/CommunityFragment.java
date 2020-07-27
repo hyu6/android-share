1568caedf31273a66d05a0837a8fde23f0dbced3
kiten46087 <592692725@qq.com>
2020-07-26 21:38:22 -0700

    Change the layout of the bottom navigation view

---
+++ app/src/main/java/edu/neu/madcourse/share/Fragment/CommunityFragment.java
@@ -0,0 +1,19 @@
+package edu.neu.madcourse.share.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import edu.neu.madcourse.share.R;
+
+public class CommunityFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_community, container, false);
+    }
+}
--- app/src/main/java/edu/neu/madcourse/share/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/share/MainActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
+import edu.neu.madcourse.share.Fragment.CommunityFragment;
 import edu.neu.madcourse.share.Fragment.HomeFragment;
 import edu.neu.madcourse.share.Fragment.NotificationFragment;
 import edu.neu.madcourse.share.Fragment.ProfileFragment;
@@ -21,13 +24,24 @@ public class MainActivity extends AppCompatActivity {
     // Main activity for selecting different fragment displaying different pages.
     BottomNavigationView bottomNavigationView;
     Fragment selectedFragment = null;
+    FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set the onClick listenr for the add button.
+        add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PostActivity.class));
+            }
+        });
 
         // Set the bottomNavigation listener.
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
@@ -59,17 +73,13 @@ public class MainActivity extends AppCompatActivity {
                         case R.id.nav_search:
                             selectedFragment = new SearchFragment();
                             break;
-                        case R.id.nav_add:
-                            // TODO: This button should be removed later
-                            // TODO: Using a floating action button instead.
-                            selectedFragment = null;
-                            startActivity(new Intent(MainActivity.this, PostActivity.class));
+                        case R.id.nav_community:
+                            selectedFragment = new CommunityFragment();
                             break;
                         case R.id.nav_fav:
                             selectedFragment = new NotificationFragment();
                             break;
                         case R.id.nav_profile:
-                            // TODO: Don't know what this line is doing.
                             SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
                             editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
                             editor.apply();
